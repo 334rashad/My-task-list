3efcd7b8a6bc8877dc096e3222f89ed63e347fb6
Rashadat Mirzayev <334rashad@gmail.com>
2021-11-01 00:00:03 +0100

    tasks list in task component

---
+++ src/components/Tasks.js
@@ -0,0 +1,20 @@
+const tasks = [
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    dat: "December 10, 2:00pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Friends meeting",
+    dat: "December 12, 5:00pm",
+    reminder: true,
+  },
+];
+
+const Tasks = () => {
+  return <div></div>;
+};
+
+export default Tasks;
